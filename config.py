0b240120a8d7483896a7c87cf0bdccfb3b60e819
Richard Mills <scripts.richard@gmail.com>
2017-01-08 10:56:43 -0700

    Updated style and increased config constants for more natural layout.

--- config.py
+++ config.py
@@ -10,6 +10,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-PER_PAGE = 3  # number of posts on index page
+PER_PAGE = 5  # number of posts on index page
 SNIPPET_LENGTH = 200   # character length for snippet for index page
-ARCHIVE_PER_PAGE = 5  # number of archive titles on archive page
+ARCHIVE_PER_PAGE = 20  # number of archive titles on archive page
