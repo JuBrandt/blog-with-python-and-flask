4d08739522365c35a37111f0b0beb9534c816d20
Richard Mills <scripts.richard@gmail.com>
2017-01-08 17:48:30 -0700

    Fixed slug creation and minor style changes.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import render_template, redirect, url_for, flash
 from app import app, db, lm
 from .models import Blog, Admin
@@ -16,23 +18,35 @@ def make_snippet(text):
     snippet = []
 
     for line in lines:
-        if line[0] == '!':
-            if can_have_image:
-                snippet.append(line)
-                can_have_image = False
+        # Check for 0 length and add newline if found -- for Markdown
+        if len(line) > 0:
+            # If line starts with '!' most likely an image
+            if line[0] == '!':
+                # Restrict the amount of images on the index page
+                if can_have_image:
+                    snippet.append(line)
+                    can_have_image = False
+            else:
+                snippet.append('\n')
         else:
             snippet.append(line)
 
+    # Remove broken html
     if snippet[-1].count('>') % 2 != 0:
         del snippet[-1]
 
-    if snippet[-1][-1] != '.':
-        if snippet[-1][-1] == ' ':
-            snippet[-1] = snippet[-1][:-1] + '...'
-        else:
-            snippet[-1] += '...'
+    snippet = '\n'.join(snippet)
 
-    return '\n'.join(snippet)
+    # Decide whether to add '...' if incomplete sentence
+    if snippet[-1] != '.':
+        snippet += '...'
+    return snippet
+
+
+def make_address(title):
+    pattern = re.compile('[a-z0-9A-Z]+')
+    address = ' '.join(re.findall(pattern, title)).replace(' ', '-').lower()
+    return address
 
 
 @app.route('/')
@@ -76,7 +90,7 @@ def edit(slug):
     if form.validate_on_submit():
         post.blog_title = form.blog_title.data
         post.blog_body = form.blog_body.data
-        post.blog_address = post.blog_title.replace(' ', '-')
+        post.blog_address = make_address(post.blog_title)
         post.blog_snippet = make_snippet(post.blog_body)
         db.session.commit()
         return redirect(url_for('success'))
@@ -145,22 +159,21 @@ def internal_error(error):
 def detail(slug):
     # For individual blog post
     post = Blog.query.filter_by(blog_address=slug).first()
-    next_post = Blog.query.filter_by(id=post.id + 1).first()
-    previous_post = Blog.query.filter_by(id=post.id - 1).first()
-    if next_post:
-        next_address = next_post.blog_address
-    else:
-        next_address = False
-    if previous_post:
-        previous_address = previous_post.blog_address
-    else:
-        previous_address = False
-    previous_post = Blog.query.filter_by(id=post.id - 1).first()
-    return render_template('detail.html',
-                           title=post.blog_title,
-                           post=post,
-                           next_address=next_address,
-                           previous_address=previous_address)
+    next_address = False
+    previous_address = False
+    if post:
+        next_post = Blog.query.filter_by(id=post.id + 1).first()
+        previous_post = Blog.query.filter_by(id=post.id - 1).first()
+        if next_post:
+            next_address = next_post.blog_address
+        if previous_post:
+            previous_address = previous_post.blog_address
+        return render_template('detail.html',
+                               title=post.blog_title,
+                               post=post,
+                               next_address=next_address,
+                               previous_address=previous_address)
+    return render_template('404.html', title='404'), 404
 
 
 @app.route('/archive')
