3e921b368132c3e42a2693d9a7d26165f238527f
Richard Mills <scripts.richard@gmail.com>
2017-02-04 17:54:47 -0700

    Removed additional login requirement.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
-from flask_login import LoginManager
 from flask_frozen import Freezer
 
 app = Flask(__name__)
 Markdown(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-lm = LoginManager()
-lm.init_app(app)
-lm.login_view = 'login'
 
 freezer = Freezer(app)
 
